29c2a3703a66354e5b76c71f8e05e408ee2785c8
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-02-17 14:55:04 +0900

    Refactor index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
 
-const App = () => {
-  return (
-    <Text>SomeText</Text>
-  );
-};
+const App = () => (
+  <Text>SomeText</Text>
+);
 
 AppRegistry.registerComponent('react_native_udemy', () => App);
